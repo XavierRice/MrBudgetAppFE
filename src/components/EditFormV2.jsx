3d7f0250b053c8d7eeb51d1a78b4e28806e0b34d
xavierrice <xavier.rice@gmail.com>
2023-11-05 21:45:32 -0500

    major edits to my edit function

--- src/components/EditFormV2.jsx
+++ src/components/EditFormV2.jsx
@@ -6,71 +6,62 @@ const EditFormV2 = ({ transactionArr }) => {
     const { id } = useParams();  // this returns the id as a string which i always forget
     const navigate = useNavigate();
 
-    const [nameError, setNameError] = useState("");
-    const [amountError, setAmountError] = useState("");
-    const [transactionName, setTransactionName] = useState("")
-    const [amount, setAmount] = useState(0)
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    let [submittedTransaction, setSubmittedTransaction] = useState(null)
-    const [editedTransaction, setEditedTransaction] = useState({
-
+    const [amountError, setAmountError] = useState("")
+    const [nameError, setNameError] = useState("")
+    const [transaction, setTransaction] = useState({
         transaction_name: "",
-        amount: null,
+        amount: 0,
         date: "",
         from: "",
         category: ""
-    });
+    } || transaction)
 
     useEffect(() => {      //I was getting some issues with resolving to the DOM so i tried a use Effect so that i would load first.
         const transactionToEdit = transactionArr.find(transId => transId.id === parseInt(id))  // so im turning the id into a number here.
-        setSubmittedTransaction(transactionToEdit)
+        setTransaction(transactionToEdit)
     }, [id]);
 
-    function handleName(event) {
-        const newTransaction = event.target.value
-        console.log(newTransaction)
-        if (newTransaction.trim() === "") {
-            setNameError("You gotta name it something!")
-        } else {
-            setNameError("")
-            setTransactionName(newTransaction.trim())
-        }
-    };
-
-    function handleAmount(event) {
-        let numberAmount = parseFloat(event.target.value)
-        if (isNaN(numberAmount) || numberAmount <= 0) {                                 // learned isNAN after trying numberAmount ===Nan which wasnt working. Give truthy/falsey value if value is NaN
-            setAmountError("Amount has got to be money of some kind")
-        } else {
-            setAmount(numberAmount)
-        }
-    };
-
     function handleTextChange(event) {
-        setEditedTransaction({
-            ...submittedTransaction,
+        const value = event.target.id
+
+        if (value === "transaction_name") {
+            const transactionName = event.target.value
+            if (transactionName.trim() == "") {
+                setNameError("You have to enter a name")
+            } else {
+                setTransaction({
+                    ...transaction,
+                    [value]: transactionName.trim()
+                });
+            }
+        };
+        if (value === "amount") {
+            let numberAmount = parseFloat(event.target.value)
+            if (isNaN(numberAmount) || numberAmount <= 0) {
+                setAmountError("Amount has got to be money of some kind")
+            } else {
+                setTransaction({
+                    ...transaction,
+                    [value]: transactionName.trim()
+                });
+            }
+        }
+        setTransaction({
+            ...transaction,
             [event.target.id]: event.target.value,
         });
     }
 
+
     const handleSubmit = async (event) => {
-        const valid = true
         event.preventDefault();
-        handleAmount();
-        handleName();
-        setIsSubmitting(valid);
-        setEditedTransaction({
-            ...submittedTransaction,
-            ...editedTransaction,
-            amount: amount,
-            transaction_name: transactionName,
-        })
+        setIsSubmitting(!isSubmitting);
 
         try {
             const API = import.meta.env.VITE_REACT_APP_API_URL;
             const response = await fetch(`${API}/transactions/${id}`, {
                 method: "PUT",
-                body: JSON.stringify(editedTransaction),
+                body: JSON.stringify(transaction),
                 headers: {
                     "Content-Type": "application/json",
                 },
@@ -85,7 +76,7 @@ const EditFormV2 = ({ transactionArr }) => {
         } catch (error) {
             console.error("Error editing transaction:", error);
         } finally {
-            console.log(editedTransaction)                                         //finally is in the try/catch family. cuz to then. Its for a block of code that will run, at the end, regardless of error/out come
+            console.log(transaction)                                         //finally is in the try/catch family. cuz to then. Its for a block of code that will run, at the end, regardless of error/out come
             setIsSubmitting(!valid);
         }
     };
@@ -94,68 +85,68 @@ const EditFormV2 = ({ transactionArr }) => {
         <div className="container">
             <h3 className="header">Transaction</h3>
             <div className="form">
-                {submittedTransaction ? (
+                {transaction ? (
 
                     <form onSubmit={handleSubmit}>
-                    <input
-                        className='form-control'
-                        name="transaction_name"
-                        type="text"
-                        id="transaction_name"
-                        value={submittedTransaction.transaction_name}
-                        onChange={handleName}
-                        placeholder="Name of the Transaction"
-                        required
+                        <input
+                            className='form-control'
+                            name="transaction_name"
+                            type="text"
+                            id="transaction_name"
+                            value={transaction.transaction_name}
+                            onChange={handleTextChange}
+                            placeholder="Name of the Transaction"
+                            required
                         />
-                    <hr></hr>
-                    {nameError != "" && <div className='error-message'>{nameError}</div>}
-                    <hr></hr>
-                    <input
-                        className='form-control'
-                        name="amount"
-                        type="number"
-                        id={"amount"}
-                        min={.01}
-                        value={submittedTransaction.amount}
-                        onChange={handleAmount}
-                        required
+                        <hr></hr>
+                        {nameError != "" && <div className='error-message'>{nameError}</div>}
+                        <hr></hr>
+                        <input
+                            className='form-control'
+                            name="amount"
+                            type="text"
+                            id={"amount"}
+                            min={.01}
+                            value={transaction.amount}
+                            onChange={handleTextChange}
+                            required
                         />
-                    <hr></hr>
-                    {amountError != "" && <div className='error-message'>{amountError}</div>}
-                    <hr></hr>
-                    <input
-                        className='form-control'
-                        name="from"
-                        type="text"
-                        id={'from'}
-                        value={submittedTransaction.from}
-                        onChange={handleTextChange}
-                        placeholder=" Where from?"
+                        <hr></hr>
+                        {amountError != "" && <div className='error-message'>{amountError}</div>}
+                        <hr></hr>
+                        <input
+                            className='form-control'
+                            name="from"
+                            type="text"
+                            id={'from'}
+                            value={transaction.from}
+                            onChange={handleTextChange}
+                            placeholder=" Where from?"
                         />
-                    <hr></hr>
-                    <select
-                        className='form-select'
-                        name="category"
-                        id={'category'}
-                        value={submittedTransaction.category}
-                        onChange={handleTextChange}
-                        required
+                        <hr></hr>
+                        <select
+                            className='form-select'
+                            name="category"
+                            id={'category'}
+                            value={transaction.category}
+                            onChange={handleTextChange}
+                            required
                         >
-                        <option value={"sampleCategory"}>Sample</option>
-                        <option value={"firstClassCategory"}>First Class</option>
-                        <option value={"Student Expense"}>Learning</option>
-                    </select>
-                </form>
-                    ): (
-                        <p>transaction loading...</p>
-                    )}
+                            <option value={"sampleCategory"}>Sample</option>
+                            <option value={"firstClassCategory"}>First Class</option>
+                            <option value={"Student Expense"}>Learning</option>
+                        </select>
+                    </form>
+                ) : (
+                    <p>transaction loading...</p>
+                )}
                 <button onClick={handleSubmit}>Submit</button>
                 <br />
                 <Link to={`/transactions`}>
                     <button>Nevermind!</button>
                 </Link>
             </div>
-            </div>
+        </div>
     );
 
 };
